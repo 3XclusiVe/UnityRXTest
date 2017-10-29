71b55d611e5c6f1b34b0fe897b9a04742963d8ae
Dmitriy Malych <malllkin007@gmail.com>
2017-10-29 17:37:54 +0300

    completed first task

---
+++ UnityRXTest/Assets/Scripts/Inputs.cs
@@ -0,0 +1,23 @@
+﻿using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+public class Inputs : MonoBehaviour
+{
+    public IObservable<Vector2> Movement
+    {
+        get;
+        private set;
+    }
+
+    private void Awake()
+    {
+        Movement = this.FixedUpdateAsObservable()
+            .Select(_ =>
+            {
+                var x = Input.GetAxis("Horizontal");
+                var y = Input.GetAxis("Vertical");
+                return new Vector2(x, y).normalized;
+            });
+    }
+}
---
+++ UnityRXTest/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UniRx;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private Inputs _inputs;
+    [SerializeField]
+    private CharacterController _character;
+    [SerializeField]
+    private float _walkSpeed;
+
+    private void Start()
+    {
+        _inputs.Movement
+            .Where(v => v != Vector2.zero)
+            .Subscribe(inputMovement =>
+                {
+                    var inputVelocity =
+                        inputMovement * _walkSpeed;
+
+                    var playerVelocity =
+                        inputVelocity.x * transform.right +
+                        inputVelocity.y * transform.forward;
+
+                    var distance =
+                        playerVelocity *
+                        Time.fixedDeltaTime;
+
+                    _character.Move(distance);
+                }
+            );
+    }
+}
